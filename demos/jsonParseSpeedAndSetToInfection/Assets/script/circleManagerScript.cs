923fc7c93a00885eb12e761ee6c8092cb3c21fe7
PIDAMI <pop-dod@rambler.ru>
2022-03-06 22:06:22 +0300

    removed not needed files

---
+++ demos/jsonParseSpeedAndSetToInfection/Assets/script/circleManagerScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+public class circleManagerScript : MonoBehaviour
+{
+
+    public Transform circlePrefab;
+    public Bounds spawnBounds;
+    public BoxCollider2D spawnArea;
+    public List<Transform> circles;
+    public int initialNumberCircles = 1;
+    public float deltaSpawn = 5f;
+
+    public static float CIRCLE_SPEED;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        // JsonSerializer serializer = new JsonSerializer();
+
+        
+        this.circles = new List<Transform>();
+        this.spawnBounds = this.spawnArea.bounds;
+        InvokeRepeating("spawnCircle", 0f, deltaSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void spawnCircle() {
+        
+        Transform circle = Instantiate(this.circlePrefab);
+        
+        float x = Random.Range(spawnBounds.min.x, spawnBounds.max.x);
+        float y = Random.Range(spawnBounds.min.y, spawnBounds.max.y);
+        circle.position = new Vector2(x,y);
+        circles.Add(circle);
+
+    }
+
+   
+}
+
