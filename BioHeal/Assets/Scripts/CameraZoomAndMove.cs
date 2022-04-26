0ae1e5c93b296c33430c70bf48bb96145803d969
Hembos <faizrahmanov.artur@mail.ru>
2022-04-26 15:27:37 +0300

    Done some refactoring

--- BioHeal/Assets/Scripts/CameraZoomAndMove.cs
+++ BioHeal/Assets/Scripts/CameraZoomAndMove.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 public class CameraZoomAndMove : MonoBehaviour
 {
+    // Position where the user clicked to move the camera
     Vector3? touch;
 
+    // Maximum and minimum value to zoom
     [SerializeField] private float zoomMin = 1;
     [SerializeField] private float zoomMax = 2;
 
+    // Map bounds
     private float mapMinX, mapMaxX, mapMinY, mapMaxY;
 
-    private void Awake() {
+    private void Awake()
+    {
         BoxCollider2D map = Resources.Load<BoxCollider2D>("Map/Map");
         mapMinX = map.bounds.center.x - map.size.x / 2;
         mapMaxX = map.bounds.center.x + map.size.x / 2;
@@ -19,38 +23,52 @@ public class CameraZoomAndMove : MonoBehaviour
 
     private void Update()
     {
+        // If user clicked on the screen, then remember the position of the click
         if (Input.GetMouseButtonDown(0))
         {
             touch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
 
+        // If user pressed with two fingers, then zoom
         if (Input.touchCount == 2)
         {
-            Touch firstTouch = Input.GetTouch(0);
-            Touch secondTouch = Input.GetTouch(1);
-
-            Vector2 firstTouchLastPos = firstTouch.position - firstTouch.deltaPosition;
-            Vector2 secondTouchLastPos = secondTouch.position - secondTouch.deltaPosition;
-
-            float distTouch = (firstTouchLastPos - secondTouchLastPos).magnitude;
-            float curDistTouch = (firstTouch.position - secondTouch.position).magnitude;
-
-            float difference = curDistTouch - distTouch;
-
-            Zoom(0.01f * difference);
-            touch = null;
+            CameraZoomForPhone();
         }
+        // If user keeps pressing, then make camera move
         else if (Input.GetMouseButton(0))
         {
-            if (!touch.HasValue)
-            {
-                touch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            }
-            Vector3 direction = touch.Value - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Camera.main.transform.position = ClampCamera(Camera.main.transform.position + direction);
+            MoveCamera();
         }
 
+        // For scroll zooming
         Zoom(Input.GetAxis("Mouse ScrollWheel"));
+    }
+
+    private void CameraZoomForPhone()
+    {
+        Touch firstTouch = Input.GetTouch(0);
+        Touch secondTouch = Input.GetTouch(1);
+
+        Vector2 firstTouchLastPos = firstTouch.position - firstTouch.deltaPosition;
+        Vector2 secondTouchLastPos = secondTouch.position - secondTouch.deltaPosition;
+
+        float distTouch = (firstTouchLastPos - secondTouchLastPos).magnitude;
+        float curDistTouch = (firstTouch.position - secondTouch.position).magnitude;
+
+        float difference = curDistTouch - distTouch;
+
+        Zoom(0.01f * difference);
+        touch = null;
+    }
+
+    private void MoveCamera()
+    {
+        if (!touch.HasValue)
+        {
+            touch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        Vector3 direction = touch.Value - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Camera.main.transform.position = ClampCamera(Camera.main.transform.position + direction);
     }
 
     private void Zoom(float increment)
