4afe715b980f78d7b6f349f30dcba6702dfbddf9
Hembos <faizrahmanov.artur@mail.ru>
2022-08-07 21:44:08 +0300

    Indent removed

--- BioHeal/Assets/Scripts/Entities/Unit.cs
+++ BioHeal/Assets/Scripts/Entities/Unit.cs
@@ -29,7 +29,6 @@ public class Unit : Alive
         {
             Vector2 delta = (Vector2)aim.entity.transform.position - myPos;
             delta.Normalize();
-
             var angle = Vector2.SignedAngle(transform.right, delta);
 
             transform.Rotate(Vector3.forward * angle);
