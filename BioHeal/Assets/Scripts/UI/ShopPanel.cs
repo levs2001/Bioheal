64c4ca42e9df1bcd0abe6a2a15065f4438fce95b
PIDAMI <pop-dod@rambler.ru>
2022-08-14 20:11:22 +0300

    added button transparency if unit cannot be bought, moved enemies panel to top left corner

--- BioHeal/Assets/Scripts/UI/ShopPanel.cs
+++ BioHeal/Assets/Scripts/UI/ShopPanel.cs
@@ -11,6 +11,8 @@ public class ShopPanel : MonoBehaviour
     
     [SerializeField] private Text textLimfo, textGranulo, textEritro;
     [SerializeField] private Text textMoneyBase;
+    [SerializeField] private GameObject buttonLimfo, buttonEritro, buttonGranulo;
+
 
     private static ShopPanel instance = null;
     public static ShopPanel Instance
@@ -28,6 +30,7 @@ public class ShopPanel : MonoBehaviour
 
     private int money;
     private Dictionary<EntityType, int> prices = new Dictionary<EntityType, int>();
+    private Dictionary<EntityType, GameObject> buttons  = new Dictionary<EntityType, GameObject>();
 
     public int Money
     {
@@ -43,7 +46,12 @@ public class ShopPanel : MonoBehaviour
     {
         ++money;
         textMoneyBase.text = $"{money}";
+        
+        UpdateButtonsTransparency();
     }
+
+    
+
     public void BuyUnit(string str)
     {
         int price = 0;
@@ -55,6 +63,7 @@ public class ShopPanel : MonoBehaviour
         {
             money -= price;
             textMoneyBase.text = $"{money}";
+            UpdateButtonsTransparency();
 
             ActionTimer actionTimer = new GameObject(entityType.ToString() + "Timer").AddComponent<ActionTimer>();
             actionTimer.Timer = sceneManager.TimeToSpawn[entityType];
@@ -62,8 +71,35 @@ public class ShopPanel : MonoBehaviour
             actionTimer.SomeAction = (() => Destroy(actionTimer.gameObject));
             actionTimer.SomeAction = (() => SoundManager.Instance.PlaySound(SoundManager.SoundType.UnitSpawn));
         }
+        
 
         SoundManager.Instance.PlaySound(SoundManager.SoundType.AnyTap);
+    }
+
+    private void UpdateButtonsTransparency()
+    {
+         
+        foreach(KeyValuePair<EntityType, int> entry in prices)
+        {
+            if (entry.Value < money)
+            {
+                ChangeButtonTransparency(entry.Key, 1.0f);
+            }
+            else
+            {
+                ChangeButtonTransparency(entry.Key, 0.5f);
+            }
+        } 
+    }
+    
+    public void ChangeButtonTransparency(EntityType buttonsEntity, float value)
+    {
+        GameObject button;
+        buttons.TryGetValue(buttonsEntity, out button);
+        var image = button.GetComponent<Image>();
+        var tempColor = image.color;
+        tempColor.a = value;
+        image.color = tempColor;
     }
 
     // Start is called before the first frame update
@@ -78,6 +114,11 @@ public class ShopPanel : MonoBehaviour
         //method returns price by reference
         prices.TryGetValue(EntityType.Erythrocyte, out price); textEritro.text = $"{price}";
         prices.TryGetValue(EntityType.Granulocyte, out price); textGranulo.text = $"{price}";
-        prices.TryGetValue(EntityType.Lymphocyte, out price); textLimfo.text = $"{price}"; 
+        prices.TryGetValue(EntityType.Lymphocyte, out price); textLimfo.text = $"{price}";
+
+        buttons.Add(EntityType.Erythrocyte, buttonEritro);
+        buttons.Add(EntityType.Granulocyte, buttonGranulo);
+        buttons.Add(EntityType.Lymphocyte, buttonLimfo);
+        UpdateButtonsTransparency(); 
     }
 }
